093d2cdd6b075d6df7664de7db34ab61b619e027
TheON2 <sniperad@naver.com>
2023-06-07 23:34:29 +0900

    Update : add write comment

--- app.py
+++ app.py
@@ -10,44 +10,45 @@ app.secret_key = 'any random string'
 
 @app.route('/')
 def home():
-    id= session.get('id',None)
-    return render_template('index.html',id=id)
+    id = session.get('id', None)
+    return render_template('index.html', id=id)
 
 
 @app.route('/write')
 def write():
     if 'id' in session:
-        id= session.get('id',None)
-        return render_template('write.html',id=id)
+        id = session.get('id', None)
+        return render_template('write.html', id=id)
     return render_template('login.html')
 
 
 @app.route('/view')
 def view():
-    id= session.get('id',None)
-    return render_template('index.html',id=id)
+    id = session.get('id', None)
+    return render_template('index.html', id=id)
 
 
 @app.route('/myview')
 def myview():
     if 'id' in session:
-        id= session.get('id',None)
-        return render_template('myview.html',id=id)
+        id = session.get('id', None)
+        return render_template('myview.html', id=id)
     return render_template('login.html')
+
 
 @app.route('/myview2')
 def myview2():
     if 'id' in session:
-        id= session.get('id',None)
-        return render_template('myview2.html',id=id)
+        id = session.get('id', None)
+        return render_template('myview2.html', id=id)
     return render_template('login.html')
 
 
 @app.route('/mypage')
 def mypage():
     if 'id' in session:
-        id= session.get('id',None)
-        return render_template('mypage.html',id=id)
+        id = session.get('id', None)
+        return render_template('mypage.html', id=id)
     return render_template('login.html')
 
 
@@ -56,12 +57,12 @@ def join():
     return render_template('join.html')
 
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         session['id'] = request.form['id']
-        id= session.get('id',None)
-        return render_template('index.html',id=id)
+        id = session.get('id', None)
+        return render_template('index.html', id=id)
     return render_template('login.html')
 
 
@@ -71,44 +72,45 @@ def logout():
     return render_template('index.html')
 
 
-@app.route("/writediary", methods=['GET','POST'])
-def writediary_post():
+@app.route("/write_diary", methods=['GET', 'POST'])
+def write_diary():
     if request.method == 'POST':
         id_receive = request.form['id_give']
         name_receive = request.form['name_give']
-        comment_receive = request.form['comment_give']
+        content_receive = request.form['content_give']
         private_receive = request.form['private_give']
         emoji_receive = request.form['emoji_give']
 
-        all_comments = list(db.diary.find({},{'_id':False}))
+        all_comments = list(db.diary.find({}, {'_id': False}))
         count = len(all_comments) + 1
 
         doc = {
-            'num':count,
-            'id':id_receive,
-            'name':name_receive,
-            'comment':comment_receive,
-            'private':private_receive,
-            'emoji':emoji_receive,
+            'num': count,
+            'id': id_receive,
+            'name': name_receive,
+            'content': content_receive,
+            'private': private_receive,
+            'emoji': emoji_receive,
         }
         db.diary.insert_one(doc)
 
         return jsonify({'msg': '전송완료!'})
-    all_comments = list(db.diary.find({},{'_id':False}))
+    all_comments = list(db.diary.find({}, {'_id': False}))
     return jsonify({'result': all_comments})
 
-@app.route("/writecomment", methods=['POST'])
+
+@app.route("/write_comment", methods=['POST'])
 def write_comment():
     name_receive = request.form['name_give']
     comment_receive = request.form['comment_give']
     num_receive = request.form['num_give']
 
-    all_comments = list(db.comment.find({},{'_id':False}))
+    all_comments = list(db.comment.find({}, {'_id': False}))
     count = len(all_comments) + 1
 
     doc = {
         'num': num_receive,
-        'id' : count,
+        'id': count,
         'name': name_receive,
         'comment': comment_receive,
     }
@@ -116,21 +118,21 @@ def write_comment():
     return jsonify({'msg': '전송완료!'})
 
 
-@app.route("/showcomment", methods=['POST'])
+@app.route("/show_comment", methods=['POST'])
 def show_comment():
     num_receive = request.form['num_give']
 
-    all_comments = list(db.diary.find({},{'_id':False}))
+    all_comments = list(db.diary.find({"num":num_receive}, {'_id': False}))
     # 받은 게시판 넘버로 해당하는 모든 댓글을 끌고와서 반환
     return jsonify({'result': all_comments})
 
 
-@app.route("/deletecomment", methods=['POST'])
+@app.route("/delete_comment", methods=['POST'])
 def delete_comment():
     id_receive = request.form['id_give']
 
     doc = {
-        'id':id_receive,
+        'id': id_receive,
     }
     db.diary.delete_one(doc)
 
